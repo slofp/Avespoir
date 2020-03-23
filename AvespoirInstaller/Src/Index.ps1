d0c3d3fe604372152f650687e47299fc9f404f73
(Fairy)Phy <Phy.0224@faircrysta.net>
2020-03-24 04:50:55 +0900

    Linux MainMenu

--- AvespoirInstaller/Src/Index.ps1
+++ AvespoirInstaller/Src/Index.ps1
@@ -1,12 +1,13 @@
 #include
 . "./Windows/Main_Menu.ps1"
+. "./Linux/Main_Menu.ps1"
 #endinclude
 
 if ([System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Windows)) {
 	MainMenu.Windows
 }
 elseif ([System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Linux)) {
-	Write-Host "This OS is not Windows and cannot be run" -ForegroundColor Red
+	MainMenu.Linux
 }
 
 Start-Sleep -Seconds 3
